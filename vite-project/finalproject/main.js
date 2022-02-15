7f1412b1d972305d1cbfe27dcf1df26d51af11c8
Will Hunnius <90058793+whunnius@users.noreply.github.com>
2022-02-15 16:01:52 -0600

    push

---
+++ vite-project/finalproject/main.js
@@ -0,0 +1,76 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.120.1/examples/jsm/controls/OrbitControls.js'
+
+
+
+
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.set(0,0,0);
+
+renderer.render(scene,camera);
+
+
+let materialArray = [];
+let texture_ft = new THREE.TextureLoader().load( 'meadow_ft.jpg');
+let texture_bk = new THREE.TextureLoader().load( 'meadow_bk.jpg');
+let texture_up = new THREE.TextureLoader().load( 'meadow_up.jpg');
+let texture_dn = new THREE.TextureLoader().load( 'meadow_dn.jpg');
+let texture_rt = new THREE.TextureLoader().load( 'meadow_rt.jpg');
+let texture_lf = new THREE.TextureLoader().load( 'meadow_lf.jpg');
+  
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+   
+for (let i = 0; i < 6; i++)
+  materialArray[i].side = THREE.BackSide;
+   
+let skyboxGeo = new THREE.BoxGeometry( 1000, 1000, 1000);
+let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+scene.add( skybox );
+
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0,0,1000);
+scene.add(pointLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', renderer);
+controls.minDistance = 0;
+controls.maxDistance = 1;
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  camera.position.z = t * -0.00001;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * 0.00006;
+}
+
+document.body.onscroll = moveCamera;
+//moveCamera();
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  renderer.render(scene,camera);
+}
+
+animate();
